9367dd17faa5a57fbf294563dac45e6361327096
xxdragomod <xxdrago@gmail.com>
2026-06-18 20:04:13 +0530

    First commit

---
+++ next.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  async rewrites() {
+    return [
+      { source: "/", destination: "/index.html" },
+      { source: "/login", destination: "/index.html" },
+      { source: "/home", destination: "/home.html" },
+      { source: "/game", destination: "/game.html" },
+      { source: "/payment", destination: "/payment.html" },
+      { source: "/profile", destination: "/profile.html" },
+      { source: "/telegram-verify", destination: "/telegram-verify.html" },
+    ]
+  },
+}
+
+export default nextConfig
